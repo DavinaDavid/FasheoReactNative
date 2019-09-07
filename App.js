3fe3498f2a70886875b8c09d582479636a7cedca
KainatRizwan <47944805+kainatrizwan@users.noreply.github.com>
2019-09-07 14:31:56 -0700

    Add files via upload
    React Navigation
    React Stack Navigation 
    Linking of pages

--- App.js
+++ App.js
@@ -1,60 +1,95 @@
+import React from 'react';
+import { View, Text, Button ,Image, StyleSheet,AppRegistry,StatusBar} from 'react-native';
+import { createAppContainer , NavigationActions, StackActions} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+class HomeScreen extends React.Component {
+   componentDidMount(){
+     setTimeout(
+        () => {
 
-import React, {Component} from 'react';
-import {createDrawerNavigator} from 'react-navigation';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  AppRegistry,
-  Text,
-  Image,
-  StatusBar,
-  TextInput,
-  Button
-} from 'react-native';
+          this.props.navigation.dispatch(StackActions.reset({
+            index: 0,
+            actions: [
+              NavigationActions.navigate({ routeName: 'Details' })
+            ],
+          }))
 
+        },5000
 
+     );
+     
+     
+   }
 
-
-export default class App extends Component {
-
-  render(){
-  return (
-    <View style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
       <View style={styles.logo}>
         <Image style={{bottom:30}}
          source = {require('./src/images/logo.png')}
         />
+        
         <Text style={{color:'#6A2976' , fontSize:23,fontWeight:"bold",bottom:20}}>Designer Clothing and Fashion</Text>
       </View>
-
-  <Mypages/>
-
     </View> 
-    
-  );
-  }
+    );
+  }  
 }
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go Back"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Home' })
+              ],
+            }))
+          }}
+        />
+      </View>
+    );
+  }  
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  Details: {
+    title:'Login Screen',
+    screen: DetailsScreen,
+  },
+}, {
+    initialRouteName: 'Home',
+});
+
+
 
 const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:'#EADCDB'
+  
   },
   logo:{
     alignItems:"center",
     flexGrow:1,
     justifyContent:'center'
+    
   }
 
 });
 
-const Mypages = new createDrawerNavigator({
-  Login:{screen: Login},
-  Home:{screen: Home}
-})
 AppRegistry.registerComponent('App',() => App)
 
+
+export default createAppContainer(AppNavigator);
